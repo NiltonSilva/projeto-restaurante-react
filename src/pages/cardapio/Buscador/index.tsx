3accf99450fbca3ed31e3abea1ec87bf12e4d1ca
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-01-27 21:15:22 -0300

    adicionado o menu e a lógica para busca de pratos.

--- src/pages/cardapio/Buscador/index.tsx
+++ src/pages/cardapio/Buscador/index.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
 interface Props {
     busca: string,
     setBusca: React.Dispatch<React.SetStateAction<string>>
@@ -5,8 +9,12 @@ interface Props {
 
 export default function Buscador({busca, setBusca}: Props) {
     return (
-        <div>
-
+        <div className={styles.buscador}>
+            <input 
+                value = {busca}
+                onChange = {(evento) => setBusca(evento.target.value)}
+            />
+        <CgSearch size={20} color="#4C4D5E" />
         </div>
     )
 }
