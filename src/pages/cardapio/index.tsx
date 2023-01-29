2bdf61362ad40404f2a019c6c9e42ad460cf5a74
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-01-28 21:08:30 -0300

    adiconado icone da barra de titulo e feito o select personalizado.

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -3,11 +3,13 @@ import {ReactComponent as Logo} from 'assets/logo.svg'
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
+import Ordenador from './ordenador';
 
 export default function Cardapio () {
 
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
 
     return (
         <main>
@@ -26,6 +28,7 @@ export default function Cardapio () {
                     />
                     <div className={styles.cardapio__filtros}>
                         <Filtros filtro={filtro} setFiltro={setFiltro} />
+                        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
                     </div>
                 </section>
             </nav>
